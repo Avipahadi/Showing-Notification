df1833284d60a44a7f642d9ae50cbb5a07dd3cf1
Arnav Sharma <68996768+Avipahadi@users.noreply.github.com>
2021-06-16 16:49:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -35,6 +35,7 @@ function notifyMe() {
             var notify = new Notification('Hi there!', {
                 body: 'How are you doing?',
                 icon: 'https://bit.ly/2DYqRrh',
+                footer: 'Mark as read',
             });
         } else {
             Notification.requestPermission().then(function (p) {
